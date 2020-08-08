358967c55788055dc2cbb054bfb03d2fbda581f9
Angel Angelov <aangeloo@gmail.com>
2020-08-08 23:01:32 +0200

    working qbic-prep1 script

--- bin/qbic-prep1.sh
+++ bin/qbic-prep1.sh
@@ -18,9 +18,6 @@ You have used $# arguments."
 	exit 1
 fi
 
-found=$(find "$1" -type d -name "barcode*" | wc -l)
-echo "$found barcode folders found"
-
 # loop through each fast folder and rename according to csv
 fastfolders=("fast5_fail" "fast5_pass" "fastq_fail" "fastq_pass")
 
@@ -35,6 +32,31 @@ do
     currentdir="$1"/$f/${col1// /}
     [ -d $currentdir ] && echo mv "$1"/$f/${col1// /} "$1"/$f/${col2// /} || echo folder ${currentdir} not found!
     done < "$2"
-    
+
 done
 
+# second pass to actually rename
+echo "To actually execute the above commands type [yes] and press [ENTER]. Otherwise just [ENTER]."
+read confirm
+
+if  [ "$confirm" = "yes" ]; then
+
+	echo "Renaming..."
+for f in ${fastfolders[@]}
+do 
+
+    # strip white spaces in or around variables read from the csv
+    # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
+    while IFS="," read col1 col2
+    do
+    # check if dir exists and rename only if so 
+    currentdir="$1"/$f/${col1// /}
+    [ -d $currentdir ] && mv -v "$1"/$f/${col1// /} "$1"/$f/${col2// /} || echo folder ${currentdir} not found!
+    done < "$2"
+
+done
+
+else
+	echo "Nothing will be renamed, quitting."
+	exit 1
+fi
