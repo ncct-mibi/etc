f9beb14f8d7b2485d2ca8c5fe53787b912b7e2dc
Angel Angelov <aangeloo@gmail.com>
2020-11-30 14:22:21 +0100

    add prokka conversion scripts

---
+++ bin/prokka_gff2saf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# produce a SAF table (for Rsubread) from a prokka-generated gff
+
+GFFIN=$1
+
+echo -e "GeneID\tChr\tStart\tEnd\tStrand\tgene_biotype\tproduct"
+cat $GFFIN | \
+gsed '/\tCDS\t/ s/$/;gene_biotype=protein coding/' | \
+gsed '/\trRNA\t/ s/$/;gene_biotype=rRNA/' | \
+gsed '/\ttRNA\t/ s/$/;gene_biotype=tRNA/' | \
+gsed '/\ttmRNA\t/ s/$/;gene_biotype=tmRNA/' | \
+gffread --table ID,@chr,@start,@end,@strand,gene_biotype,product
+# TODO output a log file with numbers of biotypes
+
+#gffread --table ID,@chr,@start,@end,@strand testdata/prokka-ID40/PROKKA_10132019.gff
