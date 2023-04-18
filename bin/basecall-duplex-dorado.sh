37bc26ca4221fb4d05c3a201c1b9e58e5d43acf0
angelovangel <aangeloo@gmail.com>
2023-04-19 00:55:09 +0200

    add dorado duplex basecaller workflow

---
+++ bin/basecall-duplex-dorado.sh
@@ -0,0 +1,60 @@
+#! /usr/bin/env bash
+
+# duplex guppy basecalling using duplex_tools to find read pairs
+# duplex_tools and dorado (GPU!) should be installed and in your path
+
+# positional args are:
+# 1 pod5 folder
+# 2 path to model to use 
+# pod5_pass must be there
+
+# outputs duplex reads to stdout
+
+set -e
+
+# check # of args
+if [ "$#" -ne 2 ]; then
+	echo "2 arguments required:
+1) path to pod5s folder
+2) path to dorado model to use
+You have used $# arguments."
+	exit 1
+fi
+
+# check if output dir exist
+if [ ! -d "basecall_duplex" ]; then
+	echo "made basecall_duplex directory..."
+    mkdir basecall_duplex
+	#exit 1
+fi
+
+# recommended workflow for dorado
+# 1. Simplex basecall with dorado (with --emit-moves)
+# 2. Pair reads
+# 3. Duplex-basecall reads
+
+# 1
+
+touch basecall_duplex/basecall_duplex.log
+date "+%Y-%m-%d %H:%M:%S" >> basecall_duplex/basecall_duplex.log
+
+echo "running dorado simplex basecaller..."
+dorado basecaller \
+    "$2" \
+    "$1" | samtools view -Sh > basecall_duplex/reads.bam
+    #--emit-moves > basecall_duplex/reads_with_moves.sam 2>&1 | tee -a basecall_duplex/basecall_duplex.log
+
+
+# 2
+echo "running duplex_tools..."
+duplex_tools pair \
+    --output_dir basecall_duplex/pairs_from_bam \
+    basecall_duplex/reads.bam 2>&1 | tee -a basecall_duplex/basecall_duplex.log
+
+# 3
+echo "running dorado duplex basecaller..."
+dorado duplex \
+    "$2" \
+    "$1" \
+    --emit-fastq \
+    --pairs basecall_duplex/pairs_from_bam/pair_ids_filtered.txt #> basecall_duplex/duplex_reads.fastq
