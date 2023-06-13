2f30250f091955b352cef6b8daae03643afa6c68
angelovangel <aangeloo@gmail.com>
2023-06-13 13:52:42 +0300

    add sequencing summary count script

--- bin/count_seq_summary.sh
+++ bin/count_seq_summary.sh
@@ -1,13 +1,16 @@
 #! /usr/bin/env ksh
 
 # pass a ONT sequencing summary file and return bases and reads (pass only)
-# use ksh to be able to return SI units magically
-
+# use ksh to be able to return SI units automagically
 
 # bases \t reads
-# awk '$12 ~ /TRUE/ {sum+=$16; count++} END {printf "%s \t %s\n", sum, count}' "${1}"
-bases=$(awk '$12 ~ /TRUE/ {sum+=$16} END {printf "%s", sum}' "${1}")
-reads=$(awk '$12 ~ /TRUE/ {count++} END {printf "%s", count}' "${1}")
+z=$(awk '$12 ~ /TRUE/ {sum+=$16; count++} END {printf "x=%s \n y=%s", sum, count}' "${1}")
+#bases=$(awk '$12 ~ /TRUE/ {sum+=$16} END {printf "%s", sum}' "${1}")
+#reads=$(awk '$12 ~ /TRUE/ {count++} END {printf "%s", count}' "${1}")
 
-printf "%#d\n" $bases
-printf "%#d\n" $reads
+# the above assigns bases and reads in awk and assigns to z, when z is evaluated x and y become shell variables
+# this way only one pass is needed 
+# https://www.theunixschool.com/2012/08/awk-passing-awk-variables-to-shell.html
+eval $z
+printf "reads,bases\n"
+printf "%#d,%#d\n" $x $y
