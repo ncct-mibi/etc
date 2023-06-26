486d60f22dc1d45297e46be780e552f9ac4da7c0
angelovangel <aangeloo@gmail.com>
2023-06-26 15:34:32 +0200

    add qscore calc to count-seq-summary

--- bin/count-seq-summary.sh
+++ bin/count-seq-summary.sh
@@ -7,11 +7,13 @@
 # get col index as they are not very consistent
 pass=$(head -1 ${2} | sed 's/\t/\n/g' | nl | grep 'passes_filtering' | cut -f 1)
 len=$(head -1 ${2} | sed 's/\t/\n/g' | nl | grep 'sequence_length_template' | cut -f 1)
+qscore=$(head -1 ${2} | sed 's/\t/\n/g' | nl | grep 'mean_qscore_template' | cut -f 1)
+#read -r pass len qscore < <(COMMAND)
 # 
 
 # bases \t reads
-gawk -v nx="${1}" -v a="$pass" -v b="$len" '
-$a == "TRUE" {sum+=$b; count++; lenarray[idx++] = $b}
+gawk -v nx="${1}" -v a="$pass" -v b="$len" -v c="$qscore" '
+$a == "TRUE" {sum+=$b; count++; qscoresum+=$c; lenarray[idx++] = $b}
 $a == "FALSE" {sum_f+=$b; count_f++; lenarray_f[idx++] = $b}
 
 
@@ -41,8 +43,8 @@ END {
         }
     }
 
-    # file, bases_pass, bases_fail, reads_pass, reads_fail, Nx_pass, Nx_fail
-    printf "%s,%s,%s,%s,%s,%s,%s\n", ARGV[1], sum, sum_f, count, count_f, nxvalue, nxvalue_f
+    # file, bases_pass, bases_fail, reads_pass, reads_fail, Nx_pass, Nx_fail, mean_qscore
+    printf "%s,%s,%s,%s,%s,%s,%s,%.2f\n", ARGV[1], sum, sum_f, count, count_f, nxvalue, nxvalue_f, qscoresum/count
     #printf "%s\n", nxvalue
 }' "${2}"
 
